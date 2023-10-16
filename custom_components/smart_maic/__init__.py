5252570c3c0d86789bb9f056df6134f5ef294e24
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2023-10-16 20:46:04 +0300

    Initial implementation

---
+++ custom_components/smart_maic/__init__.py
@@ -0,0 +1,75 @@
+"""The Smart MAIC integration."""
+from __future__ import annotations
+
+import logging
+import asyncio
+
+from homeassistant.components import mqtt
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.util.json import json_loads_object
+
+from .smart_maic import SmartMaic
+from .coordinator import SmartMaicCoordinator
+from .const import (
+    DEVICE_ID,
+    DOMAIN,
+    PREFIX,
+)
+
+PLATFORMS = [Platform.SENSOR, Platform.NUMBER]  # , Platform.SWITCH]
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Smart MAIC from a config entry."""
+    _LOGGER.debug(f"Smart MAIC entry data: {entry.data}")
+
+    if not await mqtt.async_wait_for_mqtt_client(hass):
+        raise ConfigEntryNotReady("MQTT is not available")
+
+    smart_maic = SmartMaic(entry.data)
+    coordinator = SmartMaicCoordinator(smart_maic, hass)
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+
+    json_received = False
+
+    async def wait_for_json() -> None:
+        while not json_received:
+            await asyncio.sleep(5)
+            _LOGGER.debug("Has no JSON")
+
+    @callback
+    def async_json_received(msg: mqtt.ReceiveMessage) -> None:
+        nonlocal json_received
+        json_received = True
+
+        data = json_loads_object(msg.payload)
+        _LOGGER.debug(f"MQTT data: {data}")
+        coordinator.async_set_updated_data(data)
+
+    topic = "/".join([PREFIX, entry.data[DEVICE_ID], "JSON"])
+    _LOGGER.debug(f"Listening for MQTT topic: {topic}")
+    entry.async_on_unload(await mqtt.async_subscribe(hass, topic, async_json_received))
+
+    try:
+        async with hass.timeout.async_timeout(90):
+            await wait_for_json()
+    except asyncio.TimeoutError as ex:
+        raise ConfigEntryNotReady(f"Timeout waiting for MQTT topic {topic}") from ex
+
+    _LOGGER.debug("Has JSON!")
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
---
+++ custom_components/smart_maic/smart_maic.py
@@ -0,0 +1,85 @@
+"""Smart MAIC integration."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+from urllib.parse import urlparse, urlencode
+import requests
+
+from .const import (
+    PREFIX,
+    HTTP_TIMEOUT,
+    IP_ADDRESS,
+    PIN,
+)
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SmartMaic:
+    """Smart MAIC instance."""
+
+    def __init__(self, data: dict[str, Any]) -> None:
+        """Init Smart MAIC."""
+        self._ip_address = data[IP_ADDRESS]
+        self._pin = data[PIN]
+
+    def get_wdata(self) -> dict[str, Any]:
+        """Get "wdata" for Smart MAIC component."""
+        self._login_request()
+        return self._get_request(page="getwdata").json()
+
+    def get_config(self) -> dict[str, Any]:
+        """Get config for Smart MAIC component."""
+        self._login_request()
+        return self._get_request(page="webinit").json()
+
+    def set_mqtt_config(self) -> dict[str, Any]:
+        """Set Smart MAIC MQTT config."""
+        config = self.get_config()
+
+        self._get_request(
+            page="mqtt",
+            serv=config["serv"],
+            port=config["port"],
+            uname=config["uname"],
+            **{"pass": config["pass"]},
+            mqtt_on=1,
+            mqttint=60,
+            separat=2,
+            prefix=f"{PREFIX}/",
+        )
+
+        return self.get_config()
+
+    def set_consumption(self, key: str, value: float) -> None:
+        """Set Smart MAIC consumption value."""
+        self._login_request()
+        self._get_request(page="initval", **{key: value})
+
+    def _login_request(self) -> None:
+        self._get_request(page="devlogin", devpass=self._pin)
+
+    def _get_request(self, **kwargs) -> requests.Response:
+        """Make GET request to the Smart MAIC API."""
+        url = urlparse(f"http://{self._ip_address}/")
+        url = url._replace(query=urlencode(kwargs))
+
+        _LOGGER.debug(f"Smart MAIC request: GET {url.geturl()}")
+        try:
+            r = requests.get(url.geturl(), timeout=HTTP_TIMEOUT)
+            r.raise_for_status()
+            _LOGGER.debug(f"Smart MAIC status: {r.status_code}")
+            _LOGGER.debug(f"Smart MAIC response: {r.text}")
+
+            return r
+        except TimeoutError as timeout_error:
+            raise ConnectionError from timeout_error
+        except requests.exceptions.ConnectionError as connection_error:
+            raise ConnectionError from connection_error
+        except requests.exceptions.HTTPError as http_error:
+            if http_error.response.status_code == 400:
+                return r
+            raise ConnectionError from http_error
